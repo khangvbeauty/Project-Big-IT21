1ab0dea75a8f13452925257b4d33f11974d3a5dc
Nguyễn Vĩ Khang <68845361+khangvbeauty@users.noreply.github.com>
2023-08-04 19:25:24 +0700

    Update

--- sub1-web/app.js
+++ sub1-web/app.js
@@ -109,17 +109,3 @@ channelData.forEach((channel) => {
   channelElement.href = channel.url;
   channelsSection.appendChild(channelElement);
 });
-
-$(function () {
-  //Scroll event
-  $(window).scroll(function () {
-    var scrolled = $(window).scrollTop();
-    if (scrolled > 200) $(".go-top").fadeIn("slow");
-    if (scrolled < 200) $(".go-top").fadeOut("slow");
-  });
-
-  //Click event
-  $(".go-top").click(function () {
-    $("html, body").animate({ scrollTop: "0" }, 500);
-  });
-});
